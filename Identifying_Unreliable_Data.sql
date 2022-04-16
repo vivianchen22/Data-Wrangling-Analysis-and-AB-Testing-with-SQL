03af8dcfa3e99096a3959f16425cd64a079e1951
vivianchen22 <103799715+vivianchen22@users.noreply.github.com>
2022-04-16 17:46:31 +0800

    Update Identifying_Unreliable_Data.sql

--- Identifying_Unreliable_Data.sql
+++ Identifying_Unreliable_Data.sql
@@ -1,5 +1,5 @@
 --Exercise 1: Using any methods you like determine if you can you trust this events table dsv1069.events_201701.
---HINT: There are some entire days missing
+--(HINT: There are some entire days missing)
 SELECT 
   DATE(event_time) AS DATE,
   COUNT(event_id)
