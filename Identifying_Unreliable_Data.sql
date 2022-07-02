dac1199862b728616d22a1b747c5db5f17acd58b
Vivian22 <103799715+vivianchen22@users.noreply.github.com>
2022-07-02 17:19:32 +0800

    Update Identifying_Unreliable_Data.sql

--- Identifying_Unreliable_Data.sql
+++ Identifying_Unreliable_Data.sql
@@ -1,5 +1,6 @@
 --Exercise 1: Using any methods you like determine if you can you trust this events table dsv1069.events_201701.
 --(HINT: There are some entire days missing)
+--Answer Note: The table name inlucded specific date range. also, found that the data only covered Jan 2017.
 SELECT 
   DATE(event_time) AS DATE,
   COUNT(event_id)
@@ -13,7 +14,7 @@ ORDER BY
 --Exercise 2:
 --Using any methods you like, determine if you can you trust this events table dsv1069.events_ex2. 
 --(HINT: When did we start recording events on mobile)
-
+--Answer Note: if group by date and platform, we found that mobile's events were recorded since Jan 2013.
 SELECT 
   DATE(event_time) as Date,
   platform,
@@ -28,7 +29,7 @@ ORDER BY
 
 --Exercise 3: Imagine that you need to count item views by category. You found this table item_views_by_category_temp 
 -- should you use it to answer your questiuon?
---Answer: NO the numbers of event was not matched
+--Answer Note: NO the numbers of event was not matched
 
 --SELECT
 --  SUM (view_events)
@@ -44,11 +45,12 @@ WHERE
 
 
 --Exercise 4: Using any methods you like, decide if this table is ready to be used as a source of truth.
---Anwser: couldn't find the dsv1069.raw_events table in the database
+--Anwser Note: couldn't find the dsv1069.raw_events table in the database
 SELECT 
   *
 FROM
   dsv1069.raw_events
+  
   
 --Exercise 5: Is this the right way to join orders to users? Is this the right way this join.
 --SELECT 
@@ -60,6 +62,7 @@ FROM
 --ON 
 --  orders.user_id = users.parent_user_id
 
+---Answer Note: no. not every users has parent_user_id. 
 SELECT 
   COUNT(*)
 FROM
